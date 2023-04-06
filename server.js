235f2c82072a230dbf43f673b09a2567df1db625
anhkhonglanman <quyen283hn@gmail.com>
2023-04-06 12:42:37 +0700

    up file

--- server.js
+++ server.js
@@ -15,8 +15,17 @@ const typeFile = {
     'eot': 'application/vnd.ms-fontobject'
 }
 
+function getUrl(req){
+    const urlParse = url.parse(req.url,true)
+    const pathName = urlParse.pathname
+
+    return pathName.split('/')
+}
+
+
+
 const server = http.createServer((req, res) => {
-    const pathName = url.parse(req.url, true).pathname;
+    let urlPath = url.parse(req.url).pathname;
     if (pathName === '/assets/css/style.css') {
         fs.readFile('./views/assets/style.css', (err, data) => {
             if (err) {
@@ -30,40 +39,30 @@ const server = http.createServer((req, res) => {
         });
         return;
     } else {
-        function getUrl(req){
-            const urlParse = url.parse(req.url,true)
-            const pathName = urlParse.pathname
-
-            return pathName.split('/')
-        }
-
-        const server = http.createServer((req, res) => {
-            let urlPath = url.parse(req.url).pathname;
-            const filesDefences = urlPath.match(/\.js|\.css|\.png|\.svg|\.jpg|\.ttf|\.woff|\.woff2|\.eot|\.jpeg/);
-            if(filesDefences){
-                const extension = typeFile[filesDefences[0].toString().split('.')[1]];
-                res.writeHead(200,{'Content-Type': extension});
-                fs.createReadStream(__dirname + req.url).pipe(res)
-            }else {
-                const arrPath = getUrl(req);
-                let trimPath = '';
-                if (arrPath.length > 2) {
-                    trimPath = arrPath[1] + '/' + arrPath[2];
-                } else {
-                    trimPath = arrPath[arrPath.length - 1];
-                }
-                let chosenHandler;
-                if (typeof router[trimPath] === 'undefined') {
-                    // chosenHandler = notFoundRouting.showNotFound;
-                    res.end();
-                } else {
-                    chosenHandler = router[trimPath];
-
-                    chosenHandler(req, res, arrPath[3]);
-                }
-
+        const filesDefences = urlPath.match(/\.js|\.css|\.png|\.svg|\.jpg|\.ttf|\.woff|\.woff2|\.eot|\.jpeg/);
+        if(filesDefences){
+            const extension = typeFile[filesDefences[0].toString().split('.')[1]];
+            res.writeHead(200,{'Content-Type': extension});
+            fs.createReadStream(__dirname + req.url).pipe(res)
+        }else {
+            const arrPath = getUrl(req);
+            let trimPath = '';
+            if (arrPath.length > 2) {
+                trimPath = arrPath[1] + '/' + arrPath[2];
+            } else {
+                trimPath = arrPath[arrPath.length - 1];
             }
-        })
+            let chosenHandler;
+            if (typeof router[trimPath] === 'undefined') {
+                // chosenHandler = notFoundRouting.showNotFound;
+                res.end();
+            } else {
+                chosenHandler = router[trimPath];
+
+                chosenHandler(req, res, arrPath[3]);
+            }
+
+        }
     }
 
 })
