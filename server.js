1df775a36f6960d04d1cf8baee2f6a270d57a28a
anhkhonglanman <quyen283hn@gmail.com>
2023-04-06 12:37:20 +0700

    3st

--- server.js
+++ server.js
@@ -30,7 +30,40 @@ const server = http.createServer((req, res) => {
         });
         return;
     } else {
+        function getUrl(req){
+            const urlParse = url.parse(req.url,true)
+            const pathName = urlParse.pathname
 
+            return pathName.split('/')
+        }
+
+        const server = http.createServer((req, res) => {
+            let urlPath = url.parse(req.url).pathname;
+            const filesDefences = urlPath.match(/\.js|\.css|\.png|\.svg|\.jpg|\.ttf|\.woff|\.woff2|\.eot|\.jpeg/);
+            if(filesDefences){
+                const extension = typeFile[filesDefences[0].toString().split('.')[1]];
+                res.writeHead(200,{'Content-Type': extension});
+                fs.createReadStream(__dirname + req.url).pipe(res)
+            }else {
+                const arrPath = getUrl(req);
+                let trimPath = '';
+                if (arrPath.length > 2) {
+                    trimPath = arrPath[1] + '/' + arrPath[2];
+                } else {
+                    trimPath = arrPath[arrPath.length - 1];
+                }
+                let chosenHandler;
+                if (typeof router[trimPath] === 'undefined') {
+                    // chosenHandler = notFoundRouting.showNotFound;
+                    res.end();
+                } else {
+                    chosenHandler = router[trimPath];
+
+                    chosenHandler(req, res, arrPath[3]);
+                }
+
+            }
+        })
     }
 
 })
